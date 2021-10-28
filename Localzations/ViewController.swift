4ad41fc6a9b31cd548178260023fe70dab3b90d8
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-10-28 10:43:16 +0300

    Done Localzations.

--- Localzations/ViewController.swift
+++ Localzations/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fourth: UILabel!
+    @IBOutlet weak var titleName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleName.text = "Third".localized
+        fourth.text = "Fourth".localized
+        // NSLocalizedString(, comment: "")
+        
+        let local = Locale.current
+        local.languageCode
+        
+        
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 
+}
 
+extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
 }
 
